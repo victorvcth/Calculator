d8f6d580ba0ac4203e8a1c24ec68813b8c8fb7d8
Victor Barros <victor_buzetti@hotmail.com>
2022-08-09 23:22:10 -0300

    Subindo projeto

---
+++ src/components/Button.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Button.css'
+
+export default props => {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
+    return (
+        <button 
+        onClick={e => props.click(props.label)}
+        className={classes}>
+            {props.label}
+        </button>
+    )
+}
+    
+
+
+// PODE FAZER DESSE JEITO TAMBÉM!
+{/* <button className={`
+        button
+        ${props.operation ? 'operation': '' }
+        ${props.double ? 'double' : ''}
+        ${props.triple ? 'triple' : ''}
+    `}>
+        {props.label}
+    </button> */}
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calculator from './main/Calculator';
+import './main/Calculator'
+import './index.css'
+
+
+
+
+ReactDOM.render(
+    <div>
+        <h1>Calculadora</h1>
+        <Calculator />
+    </div>
+, document.getElementById('root'));
+
